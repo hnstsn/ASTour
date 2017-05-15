dfa5abd665e08f5236aca9a0265d646587aad99f
bituser <bituser@bit>
2017-05-15 13:57:45 +0900

    로그인 처리와 메인 메뉴 부분 변경

--- ASTour/src/main/java/com/project/astour/controller/member/MemberCtrl.java
+++ ASTour/src/main/java/com/project/astour/controller/member/MemberCtrl.java
@@ -1,15 +1,30 @@
 package com.project.astour.controller.member;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("member")
 public class MemberCtrl {
 	
-	@RequestMapping("login.do")
-	public String login() {
-		return "";
+	private static final Logger logger = LoggerFactory.getLogger("MemberCtrl.class");
+	
+	// AST(CSW) : 로그인 체크 
+	@RequestMapping("loginCheck.do")
+	@ResponseBody
+	public boolean login(@RequestParam String mid, @RequestParam String mpw) {
+		logger.info("로그인 확인 컨트롤러 왔음~~~~~~~~~");
+		logger.info("넘어온 아이디 : " + mid + ", 넘어온 pw : " + mpw);
+		//아이디랑 비밀번호는 나중에 DB에서 가져와 확인해야 한다.
+		// 아이디와 비밀번호가 일치하면  true 리턴
+		if (mid.equals("abc@abc.com") && mpw.equals("1234"))
+			return true;
+		// 일치하지 않으면  false 리턴
+		return false;
 	}
 
 }
