1e9d372f7464390479bba71fd15c53f73c192c53
bitwarrior1 <jjydmz@naver.com>
2017-07-03 17:50:38 +0900

    야!

---
+++ ASTour/src/main/java/com/project/astour/controller/map/Route.java
@@ -0,0 +1,14 @@
+package com.project.astour.controller.map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Route {
+	
+	@RequestMapping("searchRoute")
+	public void searchRoute(){
+		
+	}
+	
+}
